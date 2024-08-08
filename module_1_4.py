087ad5a2ca36f4afc12ab9650699a4b858d5f468
Zipkyn <v.t.s.782003@gmail.com>
2024-08-08 17:09:50 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Напишите вашу фамилию и имя - ')
+print("В данной строке находится",len(my_string), "символа")
+print('ВЕРХНИЙ РЕГИСТ -',my_string.upper())
+print('нижний регистр -',my_string.lower())
+print('Без пробелов -',my_string.replace(' ', ''))
+print('Первая буква -',my_string[0])
+print('Последняя буква -',my_string[-1])
